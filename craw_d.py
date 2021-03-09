8cd7e42d9aa8bd1ecfe5b98ac59628695dd85dc1
bugxh1 <56824328+bugxh1@users.noreply.github.com>
2021-03-09 16:33:25 +0800

    Update craw_d.py

--- craw_d.py
+++ craw_d.py
@@ -11,11 +11,12 @@ import argparse
 
 parser = argparse.ArgumentParser() # defines the parser
 parser.add_argument('-p', help='The proxy port', dest='proxy',default='127.0.0.1:7777')
+parser.add_argument('-p2', help='The proxy port', dest='proxy2',default='')
 parser.add_argument('-u', help='targets url', dest='target_url')
 args = parser.parse_args()
 proxy = args.proxy
+proxy2 = args.proxy2
 target_url = args.target_url
-ua = UserAgent()
 ua = UserAgent(use_cache_server=False)
 
 warnings.filterwarnings(action='ignore')
@@ -26,9 +27,9 @@ def get_random_headers():
     return headers
 
 
-def main(url):
+def main(url,pro):
 	target = url
-	cmd = ["./crawlergo", "-c", "chrome-linux/chrome","-t", "20","-f","smart","--fuzz-path","--custom-headers",json.dumps(get_random_headers()), "--push-to-proxy", "http://"+proxy+"/", "--push-pool-max", "10","--output-mode", "json" , target]
+	cmd = ["./crawlergo", "-c", "chrome-linux/chrome","-t", "20","-f","smart","--fuzz-path","--custom-headers",json.dumps(get_random_headers()), "--push-to-proxy", "http://"+pro+"/", "--push-pool-max", "10","--output-mode", "json" , target]
 	rsp = subprocess.Popen(cmd, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
 	output, error = rsp.communicate()
 	try:
@@ -40,4 +41,9 @@ def main(url):
 
 
 if __name__ == '__main__':
-	main(target_url)
+	if proxy2.strip()=='':
+		main(target_url,proxy)
+	else:
+		main(target_url,proxy)
+		main(target_url,proxy2)
+	
